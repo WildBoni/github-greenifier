39c6b0593382d746f58f9ce328d76b635f3457c2
Davide Bonifacio <web@davidebonifacio.it>
2024-11-10 22:16:33 +0100

    fixing GitHub Action permissions

--- index.js
+++ index.js
@@ -3,6 +3,16 @@
 import fs from "fs";
 import path from "path";
 import inquirer from "inquirer";
+import { execSync } from "child_process";
+
+function isGitInstalled() {
+  try {
+    execSync("git --version", { stdio: "ignore" });
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
 
 async function generateCronJob() {
   // Ask the user for input
@@ -22,54 +32,67 @@ async function generateCronJob() {
   ]);
 
   // Generate the cron job YAML content
-  const cronJobYml = `name: Auto Update File
+  const cronJobYml = `
+    name: Auto Update File
 
-on:
-  schedule:
-    - cron: "0 */${hours} * * *"
+    on:
+      schedule:
+        - cron: "0 */${hours} * * *"
 
-jobs:
-  update-file:
-    runs-on: ubuntu-latest
-    env:
-      GITHUB_TOKEN: \${{ secrets.GITHUB_TOKEN }}
+    permissions:
+      content: write
 
-    steps:
-    - name: Checkout repository
-      uses: actions/checkout@v4
+    jobs:
+      update-file:
+        runs-on: ubuntu-latest
 
-    - name: Update text file
-      run: echo "X" >> dummy_file.txt
+        steps:
+        - name: Checkout repository
+          uses: actions/checkout@v4
 
-    - name: Commit changes
-      run: |
-        git config --local user.name "github-actions[bot]"
-        git config --local user.email "github-actions[bot]@users.noreply.github.com"
-        git add dummy_file.txt
-        git commit -m "Automated update - \$(date)" || echo "no changes to commit"
+        - name: Update text file
+          run: echo "\$(date)" >> dummy_file.txt
 
-    - name: Push changes
-      run: git push
-      env:
-        GITHUB_TOKEN: \${{ secrets.GITHUB_TOKEN }}
-`;
+        - name: Commit changes
+          run: |
+            git config --local user.name "github-actions[bot]"
+            git config --local user.email "github-actions[bot]@users.noreply.github.com"
+            git add dummy_file.txt
+            git commit -m "Automated update - \$(date)" || echo "no changes to commit"
 
-  const targetDir = path.join(process.cwd(), ".github", "workflows");
+        - name: Push changes
+          run: git push
+  `;
 
-  // Ensure the .github/workflows directory exists, create it if it doesn't
-  if (!fs.existsSync(targetDir)) {
-    fs.mkdirSync(targetDir, { recursive: true });
+  const projectFolderPath = path.join(process.cwd(), "github-greenifier");
+  if (!fs.existsSync(projectFolderPath)) {
+    fs.mkdirSync(projectFolderPath, { recursive: true });
   }
 
-  const filePath = path.join(targetDir, "auto_update_file.yml");
+  const githubWorkflowDir = path.join(
+    process.cwd(),
+    "github-greenifier",
+    ".github",
+    "workflows"
+  );
+  if (!fs.existsSync(githubWorkflowDir)) {
+    fs.mkdirSync(githubWorkflowDir, { recursive: true });
+  }
+
+  const githubActionFilePath = path.join(
+    githubWorkflowDir,
+    "auto_update_file.yml"
+  );
 
   // Write the cron job YAML content to the file
-  fs.writeFileSync(filePath, cronJobYml);
+  fs.writeFileSync(githubActionFilePath, cronJobYml);
 
-  console.log(`GitHub Actions cron job YAML file generated at "${filePath}"`);
+  console.log(
+    `GitHub Actions cron job YAML file generated at "${githubActionFilePath}"`
+  );
 
   // Define the path for dummy_file.txt in the root directory of the project
-  const dummyFilePath = path.join(process.cwd(), "dummy_file.txt");
+  const dummyFilePath = path.join(projectFolderPath, "dummy_file.txt");
 
   // Check if dummy_file.txt already exists, if not, create it with initial content
   if (!fs.existsSync(dummyFilePath)) {
@@ -79,6 +102,32 @@ jobs:
     );
   } else {
     console.log(`dummy_file.txt already exists at "${dummyFilePath}".`);
+  }
+
+  if (isGitInstalled()) {
+    // Initialize a Git repository
+    execSync("git init", { cwd: projectFolderPath });
+
+    // Add the test.txt file to the staging area
+    execSync("git add .", { cwd: projectFolderPath });
+
+    // Commit the changes with a message
+    try {
+      execSync('git commit -m "GitHub Greenifier initialized"', {
+        cwd: projectFolderPath,
+        stdio: "inherit",
+      });
+    } catch (error) {
+      console.error("Error committing the changes:", error.message);
+    }
+
+    console.log(
+      "Project successfully created: push it on a private GitHub repo and let Greenifier take care of the rest."
+    );
+  } else {
+    console.log(
+      'Git is not installed. The folder "github-greenifier" has been created with the test.txt file, but Git initialization and commit were skipped.'
+    );
   }
 }
 
